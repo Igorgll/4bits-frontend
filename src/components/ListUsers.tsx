c9f0f9b64d4418f373c0fb54c30a90339941aa2a
Brandy <brandysernaque@gmail.com>
2024-03-15 00:31:39 -0300

    Tela de Alterar Usuario

--- src/components/ListUsers.tsx
+++ src/components/ListUsers.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
+import { Link } from "react-router-dom";
 
 interface UserDTO {
   name: string;
@@ -62,7 +63,12 @@ const ListUsers = () => {
                     <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                   </label>
                 </td>
-                <td className="border px-4 py-2 text-white">Alterar</td>
+                <td className="border px-4 py-2 text-white"><Link
+              to="/updateUser"
+              className="text-center hover:bg-gray-700  rounded-md block">
+              Alterar
+            </Link>
+            </td>
               </tr>
             ))}
           </tbody>
