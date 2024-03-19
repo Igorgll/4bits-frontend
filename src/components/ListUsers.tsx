e6f35515bf4f2eaf96627daf48fefafae6024d31
JOHN BRANDY SERNAQUE CULQUE <john.bsculque@senacsp.edu.br>
2024-03-18 22:27:04 -0300

    Adicionando popup de alterar user

--- src/components/ListUsers.tsx
+++ src/components/ListUsers.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
-import { Link } from "react-router-dom";
+import { Button, Modal } from 'flowbite-react';
 
 interface UserDTO {
   name: string;
@@ -11,6 +11,7 @@ interface UserDTO {
 }
 
 const ListUsers = () => {
+  const [openModal, setOpenModal] = useState(false);
   const [users, setUsers] = useState<UserDTO[]>([]); // Estado para armazenar os dados dos usuários
 
   useEffect(() => {
@@ -36,16 +37,15 @@ const ListUsers = () => {
   }, []);
 
   return (
-    <Wrapper className="bg-[#111827]">
+    <Wrapper className="bg-[#111827] absolute">
       <div className="mt-4">
-      <table className="table-auto">
+        <table className="table-auto">
           <thead>
             <tr>
               <th className="px-4 py-2 text-white">Nome</th>
               <th className="px-4 py-2 text-white">Email</th>
               <th className="px-4 py-2 text-white">Grupo</th>
               <th className="px-4 py-2 text-white">Ativo</th>
-              <th className="px-4 py-2 text-white">Alterar Usuário</th>
             </tr>
           </thead>
           <tbody>
@@ -57,21 +57,60 @@ const ListUsers = () => {
                   {user.group}
                 </td>
                 <td className="border px-4 py-2 flex items-center justify-center text-white">
-                  <label className="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className="sr-only peer" />
-                    <span className="ms-3 text-sm font-medium">Ativo</span>
-                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                  </label>
+                    <label className="inline-flex items-center cursor-pointer">
+                      <input type="checkbox" value="" className="sr-only peer" />
+                      <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    </label>
                 </td>
-                <td className="border px-4 py-2 text-white"><Link
-              to="/updateUser"
-              className="text-center hover:bg-gray-700  rounded-md block">
-              Alterar
-            </Link>
-            </td>
+                <td className="border px-4 py-2 text-white">
+                  <Button onClick={() => setOpenModal(true)}>Alterar</Button>
+                </td>
               </tr>
             ))}
           </tbody>
+          <div>
+            <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+              <Modal.Header>Terms of Service</Modal.Header>
+              <Modal.Body>
+                <form className="p-4 md:p-5">
+                  <div className="grid gap-4 mb-4 grid-cols-2">
+                    <div className="col-span-2">
+                      <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                      <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" />
+                    </div>
+                    <div className="col-span-2 sm:col-span-1">
+                      <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                      <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" />
+                    </div>
+                    <div className="col-span-2 sm:col-span-1">
+                      <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                      <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <option selected>Select category</option>
+                        <option value="TV">TV/Monitors</option>
+                        <option value="PC">PC</option>
+                        <option value="GA">Gaming/Console</option>
+                        <option value="PH">Phones</option>
+                      </select>
+                    </div>
+                    <div className="col-span-2">
+                      <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                      <textarea id="description" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>
+                    </div>
+                  </div>
+                  <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                    Add new product
+                  </button>
+                </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={() => setOpenModal(false)}>I accept</Button>
+                <Button color="gray" onClick={() => setOpenModal(false)}>
+                  Decline
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </table>
       </div>
     </Wrapper>
