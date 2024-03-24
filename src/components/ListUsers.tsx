307b4b6835994efe1a76e25844813e09b7301700
igorgll <igorlimagn@gmail.com>
2024-03-23 22:36:36 -0300

    Adicionando método de alterar status do user

--- src/components/ListUsers.tsx
+++ src/components/ListUsers.tsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
-import { Label, Select, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow, TextInput } from 'flowbite-react';
+import {
+  Label,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+  TextInput,
+} from "flowbite-react";
 import { Button, Modal } from "flowbite-react";
 import SearchBar from "./SearchBar";
 import ConfirmUpdateUserDialog from "./ConfirmUpdateUserDialog";
@@ -20,6 +30,10 @@ const ListUsers = () => {
   const [selectedUser, setSelectedUser] = useState<UserDTO | null>(null); // Estado para armazenar o usuário selecionado para edição
   const [users, setUsers] = useState<UserDTO[]>([]); // Estado para armazenar os dados dos usuários
   const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const [userId, setUserId] = useState<number | undefined>(/* valor inicial */);
+  const [newStatus, setNewStatus] = useState<
+    boolean | undefined
+  >(/* valor inicial */);
 
   const fetchUsers = async () => {
     try {
@@ -38,9 +52,43 @@ const ListUsers = () => {
     }
   };
 
+  const changeUserStatus = async (userId: number, isActive: boolean) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/v1/users/isUserActive/${userId}/${isActive}`,
+        {
+          method: "PATCH",
+        }
+      );
+      if (response.ok) {
+        const updatedUsers = users.map((user) => {
+          if (user.userId === userId) {
+            return {
+              ...user,
+              isActive: isActive,
+            };
+          }
+          return user;
+        });
+        setUsers(updatedUsers);
+        console.log("User status mudado com sucesso");
+      } else {
+        console.error("Falha ao mudar o status do usuário");
+      }
+    } catch (error) {
+      console.error("Falha ao mudar o status do usuário:", error);
+    }
+  };
+
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  useEffect(() => {
+    if (userId !== undefined && newStatus !== undefined) {
+      changeUserStatus(userId, newStatus);
+    }
+  }, [userId, newStatus]);
 
   const handleOpenModal = (user: UserDTO) => {
     setSelectedUser(user);
@@ -59,33 +107,54 @@ const ListUsers = () => {
   const handleUpdateUser = async () => {
     try {
       if (!selectedUser) return;
-  
+
       const updatedUser = { ...selectedUser };
-  
+
       // Atualizar os dados do usuário com os valores dos campos de entrada
-      const nameInput = document.getElementById("name") as HTMLInputElement | null;
-      const emailInput = document.getElementById("email") as HTMLInputElement | null;
-      const cpfInput = document.getElementById("cpf") as HTMLInputElement | null;
-      const groupSelect = document.getElementById("group") as HTMLSelectElement | null;
-      const passwordInput = document.getElementById("password") as HTMLInputElement | null;
-      const confirmPasswordInput = document.getElementById("confirmPassword") as HTMLInputElement | null;
-  
-      if (!nameInput || !emailInput || !cpfInput || !groupSelect || !passwordInput || !confirmPasswordInput) {
-        console.error("Não foi possível encontrar todos os elementos necessários.");
+      const nameInput = document.getElementById(
+        "name"
+      ) as HTMLInputElement | null;
+      const emailInput = document.getElementById(
+        "email"
+      ) as HTMLInputElement | null;
+      const cpfInput = document.getElementById(
+        "cpf"
+      ) as HTMLInputElement | null;
+      const groupSelect = document.getElementById(
+        "group"
+      ) as HTMLSelectElement | null;
+      const passwordInput = document.getElementById(
+        "password"
+      ) as HTMLInputElement | null;
+      const confirmPasswordInput = document.getElementById(
+        "confirmPassword"
+      ) as HTMLInputElement | null;
+
+      if (
+        !nameInput ||
+        !emailInput ||
+        !cpfInput ||
+        !groupSelect ||
+        !passwordInput ||
+        !confirmPasswordInput
+      ) {
+        console.error(
+          "Não foi possível encontrar todos os elementos necessários."
+        );
         return;
       }
-  
+
       updatedUser.name = nameInput.value;
       updatedUser.email = emailInput.value;
       updatedUser.cpf = cpfInput.value;
       updatedUser.group = groupSelect.value;
       updatedUser.password = passwordInput.value;
-  
+
       if (passwordInput.value !== confirmPasswordInput.value) {
         console.error("Os campos de senha não correspondem.");
         return;
       }
-  
+
       // Atualize o estado selectedUser diretamente com o objeto atualizado
       setSelectedUser(updatedUser);
 
@@ -106,7 +175,6 @@ const ListUsers = () => {
       } else {
         console.error("Falha ao atualizar o usuário");
       }
-      
     } catch (error) {
       console.error("Erro ao atualizar o usuário:", error);
     }
@@ -128,24 +196,39 @@ const ListUsers = () => {
         </TableHead>
         <TableBody className="divide-y">
           {users.map((user, index) => (
-          <TableRow key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {user.name}
-            </TableCell>
-            <TableCell>{user.email}</TableCell>
-            <TableCell>{user.cpf}</TableCell>
-            <TableCell>{user.group}</TableCell>
-            <TableCell>{user.isActive ? "Inativo" : "Ativo"}</TableCell>
-            <TableCell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                onClick={() => handleOpenModal(user)} // Abrir o modal ao clicar no link "Alterar"
-              >
-                Alterar
-              </a>
-            </TableCell>
-          </TableRow>
+            <TableRow
+              key={index}
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+            >
+              <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {user.name}
+              </TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.cpf}</TableCell>
+              <TableCell>{user.group}</TableCell>
+              <TableCell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    const newStatus = !user.isActive;
+                    changeUserStatus(user.userId, newStatus);
+                  }}
+                >
+                  {user.isActive ? "Ativo" : "Inativo"}
+                </a>
+              </TableCell>
+              <TableCell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  onClick={() => handleOpenModal(user)} // Abrir o modal ao clicar no link "Alterar"
+                >
+                  Alterar
+                </a>
+              </TableCell>
+            </TableRow>
           ))}
         </TableBody>
       </Table>
@@ -153,88 +236,84 @@ const ListUsers = () => {
         <Modal show={openModal} onClose={handleCloseModal}>
           <Modal.Header />
           <Modal.Body>
-          <div className="space-y-6">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Alterar Usuário</h3>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="email" value="Email" />
+            <div className="space-y-6">
+              <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                Alterar Usuário
+              </h3>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email" value="Email" />
+                </div>
+                <TextInput id="email" value={selectedUser.email} required />
               </div>
-              <TextInput
-                id="email"
-                value={selectedUser.email}
-                required
-              />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="name" value="Nome" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="name" value="Nome" />
+                </div>
+                <TextInput id="name" placeholder={selectedUser.name} required />
               </div>
-              <TextInput
-                id="name"
-                placeholder={selectedUser.name}
-                required
-              />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="cpf" value="CPF" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="cpf" value="CPF" />
+                </div>
+                <TextInput
+                  id="cpf"
+                  value={selectedUser.cpf}
+                  onChange={(e) => {
+                    setSelectedUser({
+                      ...selectedUser,
+                      cpf: e.target.value,
+                    });
+                  }}
+                  required
+                />
               </div>
-              <TextInput
-                id="cpf"
-                value={selectedUser.cpf}
-                onChange={(e) => {
-                  setSelectedUser({
-                    ...selectedUser,
-                    cpf: e.target.value
-                  });
-                }}
-                required
-              />
-            </div>
-            <div className="max-w-full">
-            <div className="mb-2 block">
-              <Label htmlFor="group" value="Selecione o grupo" />
-            </div>
-            <Select id="group" required defaultValue={selectedUser.group}>
-              <option>Admin</option>
-              <option>Estoquista</option>
-            </Select>
-          </div>
-          <div>
-              <div className="mb-2 block">
-                <Label htmlFor="password" value="Senha" />
+              <div className="max-w-full">
+                <div className="mb-2 block">
+                  <Label htmlFor="group" value="Selecione o grupo" />
+                </div>
+                <Select id="group" required defaultValue={selectedUser.group}>
+                  <option>Admin</option>
+                  <option>Estoquista</option>
+                </Select>
               </div>
-              <TextInput
-                id="password"
-                value={selectedUser.password}
-                required
-                type="password"
-              />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="confirmPassword" value="Confirmar Senha" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password" value="Senha" />
+                </div>
+                <TextInput
+                  id="password"
+                  value={selectedUser.password}
+                  required
+                  type="password"
+                />
               </div>
-              <TextInput
-                id="confirmPassword"
-                value={selectedUser.password}
-                required
-                type="password"
-              />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="confirmPassword" value="Confirmar Senha" />
+                </div>
+                <TextInput
+                  id="confirmPassword"
+                  value={selectedUser.password}
+                  required
+                  type="password"
+                />
+              </div>
+              <div className="w-full flex justify-between">
+                <Button color="success" onClick={handleOpenConfirmDialog}>
+                  Alterar
+                </Button>
+                <Button onClick={() => setOpenModal(false)}>Cancelar</Button>
+              </div>
             </div>
-            <div className="w-full flex justify-between">
-              <Button color="success" onClick={handleOpenConfirmDialog}>Alterar</Button>
-              <Button onClick={() => setOpenModal(false)}>Cancelar</Button>
-            </div>
-          </div>
           </Modal.Body>
         </Modal>
       )}
       {selectedUser && confirmDialogOpen && (
-        <ConfirmUpdateUserDialog 
-        user={selectedUser} 
-        onClose={() => setConfirmDialogOpen(false)} 
-        onUpdateUser={handleUpdateUser}
+        <ConfirmUpdateUserDialog
+          user={selectedUser}
+          onClose={() => setConfirmDialogOpen(false)}
+          onUpdateUser={handleUpdateUser}
         />
       )}
     </Wrapper>
