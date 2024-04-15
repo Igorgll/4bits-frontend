239ef096d62d5578fac6d782188499f8278a23b3
igorgll <igorlimagn@gmail.com>
2024-04-14 21:13:23 -0300

    Ajustes description preview

--- src/components/ProductDescriptionPreview.tsx
+++ src/components/ProductDescriptionPreview.tsx
@@ -30,7 +30,7 @@ const ProductDescriptionPreview: React.FC<ProductDescriptionPreviewProps> = ({
               />
             ))}
           </FlowbiteCarousel>
-          <div className="dark:text-white flex flex-col relative">
+          <div className="dark:text-white flex flex-col h-full relative">
             <h2 className="text-4xl font-bold text-gray-900 dark:text-white mb-3">
               {productName}
             </h2>
@@ -39,17 +39,19 @@ const ProductDescriptionPreview: React.FC<ProductDescriptionPreviewProps> = ({
                 <StarIcon key={index} />
               ))}
             </div>
-            <p className="mb-16 line-clamp-6">{description}</p>
-            <h3 className="text-3xl font-bold text-gray-900 dark:text-white mb-6">
+            <p className="mb-6 line-clamp-6">{description}</p>
+            <h3 className="text-3xl font-bold text-gray-900 dark:text-white">
               R$ {price}
             </h3>
-            <Button
-              href="#"
-              className="absolute bottom-0 w-full rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-            >
-              Adicionar ao carrinho
-              <BiCart size={24} className="ms-4" />
-            </Button>
+            <div>
+              <Button
+                href="#"
+                className="mb-8 absolute bottom-0 w-full rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+              >
+                Adicionar ao carrinho
+                <BiCart size={24} className="ms-4" />
+              </Button>
+            </div>
           </div>
         </div>
       </div>
